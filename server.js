b1b79ee3667f7fdcc7007512a21f12853a469c16
Suraj121704c <nhb668912@gmail.com>
2024-04-16 23:13:07 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// Custom route for search functionality
+server.get("/post", (req, res) => {
+  const { title, name } = req.query;
+
+  // Assuming db.json contains posts
+  const posts = router.db.get("posts").value();
+
+  if (title) {
+    // Filter posts based on title if title query parameter is provided
+    const filteredPosts = posts.filter((post) =>
+      post.title.toLowerCase().includes(title.toLowerCase())
+    );
+    res.json(filteredPosts);
+  } else if (name) {
+    // Filter posts based on title if title query parameter is provided
+    const filteredPosts = posts.filter((post) =>
+      post.name.toLowerCase().includes(name.toLowerCase())
+    );
+    res.json(filteredPosts);
+  }
+});
+
+server.use(router);
+
+server.listen(8181, () => {
+  console.log("listening on port", 8181);
+});
