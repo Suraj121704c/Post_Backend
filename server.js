ced7a409931bc8aea591e157219e9d330e83fd5e
Suraj121704c <nhb668912@gmail.com>
2024-04-19 12:33:03 +0530

    final push

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 
 const server = jsonServer.create();
 
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); 
 server.use(middlewares);
 
 // Custom route for search functionality
-server.get("/posts", (req, res) => {
-  const { title, name } = req.query;
+server.get("/post", (req, res) => {
+  const { title, name, id } = req.query;
 
   // Assuming db.json contains posts
   const posts = router.db.get("posts").value();
@@ -20,12 +22,14 @@ server.get("/posts", (req, res) => {
       post.title.toLowerCase().includes(title.toLowerCase())
     );
     res.json(filteredPosts);
+    
   } else if (name) {
     // Filter posts based on title if title query parameter is provided
     const filteredPosts = posts.filter((post) =>
       post.name.toLowerCase().includes(name.toLowerCase())
     );
     res.json(filteredPosts);
+    
   } else {
     res.json(posts);
   }
